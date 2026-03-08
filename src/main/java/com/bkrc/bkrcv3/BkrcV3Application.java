1099724b3e85530ede1fe9bdcf5fb9438b019245
lsh <hack4569@naver.com>
2026-03-08 21:00:34 +0900

    Initial commit

---
+++ src/main/java/com/bkrc/bkrcv3/BkrcV3Application.java
@@ -0,0 +1,13 @@
+package com.bkrc.bkrcv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkrcV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BkrcV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bkrc/bkrcv3/BkrcV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkrc.bkrcv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkrcV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
