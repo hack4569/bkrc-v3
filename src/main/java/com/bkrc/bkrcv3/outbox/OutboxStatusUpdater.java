7bf96196d1e2b61bf89fabe812f86303e910335a
lsh <hack4569@naver.com>
2026-04-12 22:36:54 +0900

    OutboxStatusUpdater 생성

---
+++ src/main/java/com/bkrc/bkrcv3/outbox/OutboxStatusUpdater.java
@@ -0,0 +1,23 @@
+package com.bkrc.bkrcv3.outbox;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class OutboxStatusUpdater {
+
+    private final OutboxRepository outboxRepository;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void markAsFailed(Long outboxId) {
+        outboxRepository.findById(outboxId).ifPresent(Outbox::markAsFailed);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void delete(Long outboxId) {
+        outboxRepository.deleteById(outboxId);
+    }
+}
