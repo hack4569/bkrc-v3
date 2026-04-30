5e198039ef4dc48c7c066c037062136f6883fd9d
lsh <hack4569@naver.com>
2026-04-30 17:40:14 +0900

    nodeId 변경

--- src/main/java/com/bkrc/bkrcv3/common/shared/Snowflake.java
+++ src/main/java/com/bkrc/bkrcv3/common/shared/Snowflake.java
@@ -12,7 +12,7 @@ public class Snowflake {
 	private static final long maxNodeId = (1L << NODE_ID_BITS) - 1;
 	private static final long maxSequence = (1L << SEQUENCE_BITS) - 1;
 
-	private final long nodeId = RandomGenerator.getDefault().nextLong(maxNodeId + 1);
+	private final long nodeId = new java.security.SecureRandom().nextLong(maxNodeId + 1);
 	// UTC = 2024-01-01T00:00:00Z
 	private final long startTimeMillis = 1704067200000L;
 
