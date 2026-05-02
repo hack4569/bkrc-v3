fade0a23e971e73a218936e180d790d901ac21ee
lsh <hack4569@naver.com>
2026-05-02 11:09:10 +0900

    mockito 추가

--- src/test/java/com/bkrc/bkrcv3/config/TestConfig.java
+++ src/test/java/com/bkrc/bkrcv3/config/TestConfig.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @TestConfiguration
 public class TestConfig {
@@ -13,5 +14,11 @@ public class TestConfig {
     @Primary
     public RabbitTemplate rabbitTemplate() {
         return Mockito.mock(RabbitTemplate.class);
+    }
+
+    @Bean
+    @Primary
+    public StringRedisTemplate stringRedisTemplate() {
+        return Mockito.mock(StringRedisTemplate.class);
     }
 }
